f04b468a6f8573bbfeaa0cd9b741e4d77cacc77e
RJ <rjaakash07@gmail.com>
2025-11-20 03:57:19 +0530

    Revise log messages in build.sh
    Updated log messages to include README link and format dependencies and skipped sections.

--- build.sh
+++ build.sh
@@ -165,14 +165,13 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 "${BUILD_DIR}")" ]; then abort "All builds failed."; fi
 
-log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
-log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
-log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
+log "\n- [README](https://github.com/rjaakash/RV-Apps/blob/main/README.md)"
+log "\n**Dependencies** 👇"
 log "$(cat "$TEMP_DIR"/*-rv/changelog.md)"
 
 SKIPPED=$(cat "$TEMP_DIR"/skipped 2>/dev/null || :)
 if [ -n "$SKIPPED" ]; then
-	log "\nSkipped:"
+	log "\n**Skipped** 👇"
 	log "$SKIPPED"
 fi
 
